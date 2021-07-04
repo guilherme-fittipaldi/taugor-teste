1f20a1fa814b5f1f0453bd71847f368f2a540afa
unknown <guilherme.fittipaldi6@gmail.com>
2021-07-04 11:10:39 -0300

    try catch on add task

--- src/components/tasks/AddTask.jsx
+++ src/components/tasks/AddTask.jsx
@@ -5,47 +5,53 @@ import { useAuth } from "../../contexts/AuthContext";
 
 function AddTask({ setAddTask }) {
   const { currentUser } = useAuth();
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [status, setStatus] = useState("Pendente");
   const [file, setFile] = useState(null);
+  const [title, setTitle] = useState("");
+  const [status, setStatus] = useState("Pendente");
+  const [description, setDescription] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    var newFile = file;
-    var storageRef = firebase.storage().ref("files/" + newFile.toString());
-    var uploadTask = storageRef.put(newFile);
-    uploadTask.on(
-      "state_changed",
-      function (snapshot) {
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("upload is " + progress + " done");
-      },
-      function (error) {
-        console.log(error.message);
-      },
-      function () {
-        uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-          firebase
-            .firestore()
-            .collection("Task")
-            .add({
-              title,
-              description,
-              status,
-              user: currentUser.email,
-              fileUrl: downloadURL,
-            })
-            .then(() => {
-              setTitle("");
-              setDescription("");
-              setStatus("Pendente");
-              setFile(null);
-            });
-        });
-      }
-    );
+    try {
+      var newFile = file;
+      var storageRef = firebase.storage().ref("files/" + newFile.toString());
+      var uploadTask = storageRef.put(newFile);
+      uploadTask.on(
+        "state_changed",
+        function (snapshot) {
+          var progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("upload is " + progress + " done");
+        },
+        function (error) {
+          console.log(error.message);
+        },
+        function () {
+          uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+            firebase
+              .firestore()
+              .collection("Task")
+              .add({
+                title,
+                description,
+                status,
+                user: currentUser.email,
+                fileUrl: downloadURL,
+                editHistory: "",
+              })
+              .then(() => {
+                setTitle("");
+                setDescription("");
+                setStatus("Pendente");
+                setFile(null);
+              });
+          });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+    }
 
     setAddTask(false);
   }
@@ -59,6 +65,7 @@ function AddTask({ setAddTask }) {
             type="text"
             value={title}
             onChange={(e) => setTitle(e.currentTarget.value)}
+            required
           />
         </div>
         <div>
@@ -68,6 +75,7 @@ function AddTask({ setAddTask }) {
             type="textarea"
             value={description}
             onChange={(e) => setDescription(e.currentTarget.value)}
+            required
             style={{ resize: "none" }}
           />
         </div>
@@ -78,6 +86,7 @@ function AddTask({ setAddTask }) {
             onChange={(event) =>
               setStatus(event.target.options[event.target.selectedIndex].text)
             }
+            required
           >
             <option value="pendent">Pendente</option>
             <option value="inProgress">Em andamento</option>
@@ -93,6 +102,7 @@ function AddTask({ setAddTask }) {
             // value={file}
             onChange={(e) => setFile(e.currentTarget.files[0])}
             style={{ resize: "none" }}
+            required
           />
         </div>
         <button>Adicionar tarefa</button>
